3d1e33c5e7f0d0ec9d7abaf5bd8461c4b5a0ce64
Rvjais <ranveerjais9984@gmail.com>
2025-05-27 05:05:59 +0530

    click sound added

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
     let menuItems = document.querySelectorAll(".menu-items");
     let currentIndex = 0;
+    const sound = new Audio("/Ipod/sound.mp3");
   
     function highlightItem(index) {
         menuItems.forEach((item, i) => {
-            item.style.backgroundColor = i === index ? "lightgray" : "transparent";
+            item.style.backgroundColor = i === index ? "blue" : "transparent";
         });
     }
 
     document.querySelector(".left").addEventListener("click", function () {
         currentIndex = currentIndex > 0 ? currentIndex - 1 : menuItems.length - 1;
         highlightItem(currentIndex);
+        sound.play()
     });
 
     document.querySelector(".right").addEventListener("click", function () {
         currentIndex = currentIndex < menuItems.length - 1 ? currentIndex + 1 : 0;
         highlightItem(currentIndex);
+         sound.play()
     });
 
     document.querySelector(".pause button").addEventListener("click", function () {
         menuItems[currentIndex].click();
+         sound.play()
     });
 
     
@@ -31,6 +35,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function playSongs() {
     songs.play()
+    
   }
-  play.addEventListener("click", playSongs())
+  play.addEventListener("click", playSongs)
+
 
